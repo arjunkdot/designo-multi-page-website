bacd1afbd2136ac1c441d2cc9a108c46f16d4447
ArjunCybro <78548960+arjunkdot@users.noreply.github.com>
2023-03-10 21:39:35 +0530

    Header & footer loaded.

---
+++ build/js/app.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * Loads the HTML page in the given path.
+ * @param {string} path - URL to the target file.
+ */
+function loadPath(path) {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", path, false);
+    xmlhttp.send();
+    return xmlhttp.responseText;
+}
+/**
+ * Loads header & footer
+ */
+function loadHeaderAndFooter() {
+    let headerEl = document.getElementById('header');
+    let footerEl = document.getElementById('footer');
+    if (headerEl) {
+        headerEl.innerHTML = loadPath('/common/header.html');
+    }
+    if (footerEl) {
+        footerEl.innerHTML = loadPath('/common/footer.html');
+    }
+}
+// Call the function so that the header and footer are loaded.
+loadHeaderAndFooter();
